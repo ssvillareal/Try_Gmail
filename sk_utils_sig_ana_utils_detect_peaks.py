8dc53f910ec1a95ec1d4e2dac8cf3b29e6a8fce7
ssvillareal <62572388+ssvillareal@users.noreply.github.com>
2020-03-23 16:23:31 -0500

    from AI-Machine Learning/local_files
    simple data file (8 lines of (x,y) pairs) and three py utility files

---
+++ sk_utils_sig_ana_utils_detect_peaks.py
@@ -0,0 +1,344 @@
+#this file combines the code from sk_utils.py, signal_analysis_utils.py, and detect_peaks.py
+#N.B: this code was assembled in this ("") order
+#from sk_utils.py
+
+import pandas as pd
+import numpy as np
+import time
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+from IPython.display import display
+
+from sklearn.preprocessing import LabelEncoder
+
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.gaussian_process import GaussianProcessClassifier
+
+###
+#next is dict_classifiers and def's from sk_utils
+
+dict_classifiers = {
+    "Gradient Boosting Classifier": GradientBoostingClassifier(),
+    "Random Forest": RandomForestClassifier(),
+    "Logistic Regression": LogisticRegression(),
+    "Nearest Neighbors": KNeighborsClassifier(),
+    "Decision Tree": DecisionTreeClassifier(),
+    "Linear SVM": SVC(),
+    "Neural Net": MLPClassifier(alpha = 1),
+    "Naive Bayes": GaussianNB(),   
+    "AdaBoost": AdaBoostClassifier(),
+    "Gaussian Process": GaussianProcessClassifier()
+}
+   
+def batch_classify(X_train, Y_train, X_test, Y_test, no_classifiers = 5, verbose = True):
+    """
+    This method, takes as input the X, Y matrices of the Train and Test set.
+    And fits them on all of the Classifiers specified in the dict_classifier.
+    Usually, the SVM, Random Forest and Gradient Boosting Classifier take quiet some time to train. 
+    So it is best to train them on a smaller dataset first and 
+    decide whether you want to comment them out or not based on the test accuracy score.
+    """
+    
+    dict_models = {}
+    for classifier_name, classifier in list(dict_classifiers.items())[:no_classifiers]:
+        t_start = time.clock()
+        classifier.fit(X_train, Y_train)
+        t_end = time.clock()
+        
+        t_diff = t_end - t_start
+        train_score = classifier.score(X_train, Y_train)
+        test_score = classifier.score(X_test, Y_test)
+        
+        dict_models[classifier_name] = {'model': classifier, 'train_score': train_score, 'test_score': test_score, 'train_time': t_diff}
+        if verbose:
+            print("trained {c} in {f:.2f} s".format(c=classifier_name, f=t_diff))
+    return dict_models
+
+####
+
+def label_encode(df, list_columns):
+    """
+    This method one-hot encodes all column, specified in list_columns
+    
+    """
+    for col in list_columns:
+        le = LabelEncoder()
+        col_values_unique = list(df[col].unique())
+        le_fitted = le.fit(col_values_unique)
+
+        col_values = list(df[col].values)
+        le.classes_
+        col_values_transformed = le.transform(col_values)
+        df[col] = col_values_transformed
+
+def get_train_test(df, y_col, x_cols, ratio):
+    """ 
+    This method transforms a dataframe into a train and test set, for this you need to specify:
+    1. the ratio train : test (usually 0.7)
+    2. the column with the Y_values
+    """
+    mask = np.random.rand(len(df)) < ratio
+    df_train = df[mask]
+    df_test = df[~mask]
+       
+    Y_train = df_train[y_col].values
+    Y_test = df_test[y_col].values
+    X_train = df_train[x_cols].values
+    X_test = df_test[x_cols].values
+    return df_train, df_test, X_train, Y_train, X_test, Y_test
+    
+def display_dict_models(dict_models, sort_by='test_score'):
+    cls = [key for key in dict_models.keys()]
+    test_s = [dict_models[key]['test_score'] for key in cls]
+    training_s = [dict_models[key]['train_score'] for key in cls]
+    training_t = [dict_models[key]['train_time'] for key in cls]
+    
+    df_ = pd.DataFrame(data=np.zeros(shape=(len(cls),4)), columns = ['classifier', 'train_score', 'test_score', 'train_time'])
+    for ii in range(0,len(cls)):
+        df_.loc[ii, 'classifier'] = cls[ii]
+        df_.loc[ii, 'train_score'] = training_s[ii]
+        df_.loc[ii, 'test_score'] = test_s[ii]
+        df_.loc[ii, 'train_time'] = training_t[ii]
+    
+    display(df_.sort_values(by=sort_by, ascending=False))
+    
+def display_corr_matrix(df):
+    correlation_matrix = df.corr()
+    fig, ax = plt.subplots(figsize=(10,10))
+    ax = sns.heatmap(correlation_matrix, vmax=1, square=True,annot=False,cmap='RdYlGn')
+    plt.title('Correlation matrix between the features')
+    plt.colorbar(fig, orientation='vertical', fraction=0.03, label = 'Correlation strength')
+    plt.show()
+
+def display_corr_with_col(df, col):
+    correlation_matrix = df.corr()
+    correlation_type = correlation_matrix[col].copy()
+    abs_correlation_type = correlation_type.apply(lambda x: abs(x))
+    desc_corr_values = abs_correlation_type.sort_values(ascending=False)
+    y_values = list(desc_corr_values.values)[1:]
+    x_values = range(0,len(y_values))
+    xlabels = list(desc_corr_values.keys())[1:]
+    fig, ax = plt.subplots(figsize=(8,8))
+    ax.bar(x_values, y_values)
+    ax.set_title('The correlation of all features with {}'.format(col), fontsize=20)
+    ax.set_ylabel('Pearson correlatie coefficient [abs waarde]')
+    plt.xticks(x_values, xlabels, rotation='vertical')
+    plt.show()
+###
+#end of dict_classifiers and def's from sk_utils
+
+#already imported in sk_utils.py
+#import numpy as np
+
+from scipy.signal import welch
+from scipy.fftpack import fft
+from scipy import signal
+
+#copied detect_peaks directly into this file
+#from detect_peaks import detect_peaks
+
+def get_values(y_values, T, N, f_s):
+    y_values = y_values
+    x_values = [(1/f_s) * kk for kk in range(0,len(y_values))]
+    return x_values, y_values
+
+def get_fft_values(y_values, T, N, f_s):
+    f_values = np.linspace(0.0, 1.0/(2.0*T), N//2)
+    fft_values_ = fft(y_values)
+    fft_values = 2.0/N * np.abs(fft_values_[0:N//2])
+    return f_values, fft_values
+
+def get_psd_values(y_values, T, N, f_s):
+    f_values, psd_values = welch(y_values, fs=f_s)
+    return f_values, psd_values
+
+def autocorr(x):
+    result = np.correlate(x, x, mode='full')
+    return result[len(result)//2:]
+ 
+def get_autocorr_values(y_values, T, N, f_s):
+    autocorr_values = autocorr(y_values)
+    x_values = np.array([T * jj for jj in range(0, N)])
+    return x_values, autocorr_values
+    
+def get_first_n_peaks(x,y,no_peaks=5):
+    x_, y_ = list(x), list(y)
+    if len(x_) >= no_peaks:
+        return x_[:no_peaks], y_[:no_peaks]
+    else:
+        missing_no_peaks = no_peaks-len(x_)
+        return x_ + [0]*missing_no_peaks, y_ + [0]*missing_no_peaks
+    
+def get_features(x_values, y_values, mph):
+    indices_peaks = detect_peaks(y_values, mph=mph)
+    peaks_x, peaks_y = get_first_n_peaks(x_values[indices_peaks], y_values[indices_peaks])
+    return peaks_x + peaks_y
+ 
+def extract_features_labels(dataset, labels, T, N, f_s, denominator):
+    percentile = 5
+    list_of_features = []
+    list_of_labels = []
+    for signal_no in range(0, len(dataset)):
+        features = []
+        list_of_labels.append(labels[signal_no])
+        for signal_comp in range(0,dataset.shape[2]):
+            signal = dataset[signal_no, :, signal_comp]
+            
+            signal_min = np.nanpercentile(signal, percentile)
+            signal_max = np.nanpercentile(signal, 100-percentile)
+            #ijk = (100 - 2*percentile)/10
+            mph = signal_min + (signal_max - signal_min)/denominator
+            
+            features += get_features(*get_psd_values(signal, T, N, f_s), mph)
+            features += get_features(*get_fft_values(signal, T, N, f_s), mph)
+            features += get_features(*get_autocorr_values(signal, T, N, f_s), mph)
+        list_of_features.append(features)
+    return np.array(list_of_features), np.array(list_of_labels)
+
+# Copied From http://nbviewer.ipython.org/github/demotu/BMC/blob/master/notebooks/DetectPeaks.ipynb
+# Thank you Marcos Duarte
+
+def detect_peaks(x, mph=None, mpd=1, threshold=0, edge='rising',
+                 kpsh=False, valley=False, show=False, ax=None):
+
+    """Detect peaks in data based on their amplitude and other features.
+
+    Parameters
+    ----------
+    x : 1D array_like
+        data.
+    mph : {None, number}, optional (default = None)
+        detect peaks that are greater than minimum peak height.
+    mpd : positive integer, optional (default = 1)
+        detect peaks that are at least separated by minimum peak distance (in
+        number of data).
+    threshold : positive number, optional (default = 0)
+        detect peaks (valleys) that are greater (smaller) than `threshold`
+        in relation to their immediate neighbors.
+    edge : {None, 'rising', 'falling', 'both'}, optional (default = 'rising')
+        for a flat peak, keep only the rising edge ('rising'), only the
+        falling edge ('falling'), both edges ('both'), or don't detect a
+        flat peak (None).
+    kpsh : bool, optional (default = False)
+        keep peaks with same height even if they are closer than `mpd`.
+    valley : bool, optional (default = False)
+        if True (1), detect valleys (local minima) instead of peaks.
+    show : bool, optional (default = False)
+        if True (1), plot data in matplotlib figure.
+    ax : a matplotlib.axes.Axes instance, optional (default = None).
+
+    Returns
+    -------
+    ind : 1D array_like
+        indeces of the peaks in `x`.
+
+    Notes
+    -----
+    The detection of valleys instead of peaks is performed internally by simply
+    negating the data: `ind_valleys = detect_peaks(-x)`
+    
+    The function can handle NaN's 
+
+    See this IPython Notebook [1]_.
+
+    References
+    ----------
+    .. [1] http://nbviewer.ipython.org/github/demotu/BMC/blob/master/notebooks/DetectPeaks.ipynb
+
+    Examples
+    --------
+    >>> from detect_peaks import detect_peaks
+    >>> x = np.random.randn(100)
+    >>> x[60:81] = np.nan
+    >>> # detect all peaks and plot data
+    >>> ind = detect_peaks(x, show=True)
+    >>> print(ind)
+
+    >>> x = np.sin(2*np.pi*5*np.linspace(0, 1, 200)) + np.random.randn(200)/5
+    >>> # set minimum peak height = 0 and minimum peak distance = 20
+    >>> detect_peaks(x, mph=0, mpd=20, show=True)
+
+    >>> x = [0, 1, 0, 2, 0, 3, 0, 2, 0, 1, 0]
+    >>> # set minimum peak distance = 2
+    >>> detect_peaks(x, mpd=2, show=True)
+
+    >>> x = np.sin(2*np.pi*5*np.linspace(0, 1, 200)) + np.random.randn(200)/5
+    >>> # detection of valleys instead of peaks
+    >>> detect_peaks(x, mph=0, mpd=20, valley=True, show=True)
+
+    >>> x = [0, 1, 1, 0, 1, 1, 0]
+    >>> # detect both edges
+    >>> detect_peaks(x, edge='both', show=True)
+
+    >>> x = [-2, 1, -2, 2, 1, 1, 3, 0]
+    >>> # set threshold = 2
+    >>> detect_peaks(x, threshold = 2, show=True)
+    """
+
+    x = np.atleast_1d(x).astype('float64')
+    if x.size < 3:
+        return np.array([], dtype=int)
+    if valley:
+        x = -x
+    # find indices of all peaks
+    dx = x[1:] - x[:-1]
+    # handle NaN's
+    indnan = np.where(np.isnan(x))[0]
+    if indnan.size:
+        x[indnan] = np.inf
+        dx[np.where(np.isnan(dx))[0]] = np.inf
+    ine, ire, ife = np.array([[], [], []], dtype=int)
+    if not edge:
+        ine = np.where((np.hstack((dx, 0)) < 0) & (np.hstack((0, dx)) > 0))[0]
+    else:
+        if edge.lower() in ['rising', 'both']:
+            ire = np.where((np.hstack((dx, 0)) <= 0) & (np.hstack((0, dx)) > 0))[0]
+        if edge.lower() in ['falling', 'both']:
+            ife = np.where((np.hstack((dx, 0)) < 0) & (np.hstack((0, dx)) >= 0))[0]
+    ind = np.unique(np.hstack((ine, ire, ife)))
+    # handle NaN's
+    if ind.size and indnan.size:
+        # NaN's and values close to NaN's cannot be peaks
+        ind = ind[np.in1d(ind, np.unique(np.hstack((indnan, indnan-1, indnan+1))), invert=True)]
+    # first and last values of x cannot be peaks
+    if ind.size and ind[0] == 0:
+        ind = ind[1:]
+    if ind.size and ind[-1] == x.size-1:
+        ind = ind[:-1]
+    # remove peaks < minimum peak height
+    if ind.size and mph is not None:
+        ind = ind[x[ind] >= mph]
+    # remove peaks - neighbors < threshold
+    if ind.size and threshold > 0:
+        dx = np.min(np.vstack([x[ind]-x[ind-1], x[ind]-x[ind+1]]), axis=0)
+        ind = np.delete(ind, np.where(dx < threshold)[0])
+    # detect small peaks closer than minimum peak distance
+    if ind.size and mpd > 1:
+        ind = ind[np.argsort(x[ind])][::-1]  # sort ind by peak height
+        idel = np.zeros(ind.size, dtype=bool)
+        for i in range(ind.size):
+            if not idel[i]:
+                # keep peaks with the same height if kpsh is True
+                idel = idel | (ind >= ind[i] - mpd) & (ind <= ind[i] + mpd) \
+                    & (x[ind[i]] > x[ind] if kpsh else True)
+                idel[i] = 0  # Keep current peak
+        # remove the small peaks and sort back the indices by their occurrence
+        ind = np.sort(ind[~idel])
+
+    if show:
+        if indnan.size:
+            x[indnan] = np.nan
+        if valley:
+            x = -x
+        _plot(x, mph, mpd, threshold, edge, valley, ax, ind)
+
+    return ind
